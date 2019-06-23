bb34f8dd02964844499c5838a8567bf8cfcc8de2
Jared Burrows <jaredsburrows@gmail.com>
2019-06-23 13:50:13 -0700

    update project

--- ios-gradle-swift-template/controller/FirstViewController.swift
+++ ios-gradle-swift-template/controller/FirstViewController.swift
@@ -1,11 +1,3 @@
-//
-//  FirstViewController.swift
-//  ios-gradle-swift-template
-//
-//  Created by noname on 4/17/16.
-//  Copyright © 2016 Jared Burrows. All rights reserved.
-//
-
 import UIKit
 
 class FirstViewController: UIViewController {
